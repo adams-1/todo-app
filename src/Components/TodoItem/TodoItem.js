db90552437c5db55d41810976ab721cd7db69b3d
rage <obliviousness@gmail.com>
2020-01-17 22:06:44 +0200

    finished with UI

--- src/Components/TodoItem/TodoItem.js
+++ src/Components/TodoItem/TodoItem.js
@@ -4,9 +4,14 @@ export default class TodoItem extends React.Component {
 
     render() {
         return (
-
             <div>
-                Hello from TodoItem Component
+               <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                   <h6>title</h6>
+                   <div className="todo-icon">
+                       <span className="mx-2 text-success"> e </span>
+                       <span className="mx-2 text-danger">x</span>
+                   </div>
+               </li>
             </div>
         )
     }
--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -6,8 +6,15 @@ export default class TodoList extends React.Component {
     render() {
         return (
             <div>
-                 Hello from TodoList Component
+                <ul className="list-group my-5">
+                <h3 className="text-capitalize text-center"> todo list</h3>
                 <TodoItem />
+                <button
+                    type="button"
+                    className="btn btn-danger btn-block text-capitalize mt5"
+                > clear list
+                </button> 
+                </ul>
             </div>
         );
     }
